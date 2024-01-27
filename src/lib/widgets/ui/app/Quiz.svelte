8bdd586d456af22c6b4bb530a87f5df596af2773
DarkDev <darkdev_brand@riseup.net>
2024-01-28 01:14:10 +0200

    refactoring text block

--- src/lib/widgets/ui/app/Quiz.svelte
+++ src/lib/widgets/ui/app/Quiz.svelte
@@ -5,12 +5,7 @@
 </script>
 
 <!-- <div class="flex flex-col w-full mx-auto mb-2 ">
-        <div class="mb-5 border-b border-gray-200">
-            <div class="flex flex-wrap items-baseline -mt-2">
-                <h5>12-03-2021</h5>
-                <p class="mt-1 ml-2">Transitions</p>
-            </div>
-        </div>
+
         <h1>Page transitions are a fundamental part of website navigation and user experience.</h1>
         <p>Through the lens of a set of certitudes based on deductions founded on false premise.</p>
         <p>Turd polishing put a record on and see who dances, dog and pony show, nor one-sheet. Ensure to follow requirements when developing solutions three-martini lunch, that jerk from finance really threw me under the bus. Bob called an all-hands this afternoon.</p>
@@ -21,28 +16,42 @@
         <p>Gain traction make it more corporate please we need to harvest synergy effects not enough bandwidth, and we want to empower the team with the right tools and guidance to uplevel our craft and build better nor low-hanging fruit the right info at the right time to the right people.</p>
     </div> -->
 
-<div class="max-w-3xl">
+<div class="h-screen max-w-3xl">
 	<!-- <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure> -->
 	{#each questions as question, i}
 		{#if i == quizStep}
-			<div class="prose-md prose text-left">
-				<div class="flex flex-col ">
-					<h2 class="">{question.title}</h2>
+			<div class="prose-md prose flex flex-col px-8  text-left ">
+				<div class="mx-auto mb-2 w-[160px] border-b border-gray-200">
+					<div class="-mt-2 flex flex-wrap items-baseline justify-center text-neutral-700">
+						<h5 class="">Question</h5>
+						<p class="mt-1 ml-2">{question.id} / {questions.length}</p>
+					</div>
+				</div>
+				<h3 class="mt-4 text-center text-3xl font-bold text-gray-800">{question.title}</h3>
 
-					<div class="">
+				<div class="absolute left-0 bottom-6 flex flex-col w-full content-center">
 						<button
 							on:click={() => {
-								quizStep = quizStep - 1;
+								quizStep = quizStep + 1;
 							}}
-							class="btn-outline btn-primary btn">{question.options.agree.text}</button
+							class="btn-outline mx-auto btn-primary btn-wide btn mb-4"
+							>{question.options.agree.text}</button
 						>
 						<button
 							on:click={() => {
 								quizStep = quizStep + 1;
 							}}
-							class="btn-outline btn-primary btn">{question.options.disagree.text}</button
+							class="btn-outline mx-auto  btn-primary btn-wide btn">{question.options.disagree.text}</button
 						>
-					</div>
+
+						<div class="p-8 mx-auto">
+							<button
+								on:click={() => (quizStep = quizStep + 1)}
+								class="mt-4   inline-flex items-center font-thin text-neutral-800 hover:text-neutral-600 lg:mb-0"
+							>
+								Skip this question »
+							</button>
+						</div>
 				</div>
 			</div>
 		{/if}

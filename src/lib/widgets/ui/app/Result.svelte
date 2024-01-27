98c312f121c1d7363b722b76af057a19cbfc42f2
DarkDev <darkdev_brand@riseup.net>
2024-01-28 01:23:06 +0200

    add return button

--- src/lib/widgets/ui/app/Result.svelte
+++ src/lib/widgets/ui/app/Result.svelte
@@ -1,5 +1,5 @@
 <script>
-	import ArrowLeft from 'carbon-icons-svelte/lib/Add.svelte';
+	import ArrowLeft  from 'carbon-icons-svelte/lib/ArrowLeft.svelte';
 	import { StatLine } from '$lib/shared';
 
 	const report = [
@@ -14,24 +14,45 @@
 		{
 			title: 'Political',
 			resultsProcents: [86, 20]
-		},
-
+		}
 	];
 	// export let data;
 </script>
 
-<div class="lg:px-4 py-8">
+<div class="lg:py-8 lg:px-4">
 	<!-- <button class="btn-square btn">
 		<ArrowLeft size={16}/>
 
 	</button> -->
 
 	<div
-		class=" prose-blue prose mx-auto flex flex-col  items-center rounded-t-2xl bg-neutral-200 px-6 py-8  lg:max-w-7xl lg:px-8"
+		class=" prose-blue prose mx-auto  items-center rounded-t-2xl bg-neutral-200 px-6 py-8  lg:max-w-7xl lg:px-8"
 	>
+		<button class="h-10 w-10 mb-6 rounded-2xl bg-white p-1">
+          			<ArrowLeft  fill="bg-gray-800" size={18} class='mx-auto'/>
+
+		</button>
+
+
+		<!-- <button class="btn-outline bg-white btn-square btn  mb-2">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M6 18L18 6M6 6l12 12"
+			/></svg
+		>
+	</button> -->
+
 		<div class="mx-auto flex w-full max-w-3xl flex-col text-left">
 			<div class="mx-auto w-full ">
-				<h1 class="text-neutral-800">Here is your result.</h1>
+				<h1 class="mb-0 text-neutral-800">Here is your result.</h1>
 				<!-- <h2>A small headline to switch your visitors into users.</h2> -->
 				<p class="text-neutral-700">
 					Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
@@ -40,27 +61,31 @@
 			</div>
 
 			<div class="flex flex-row justify-between">
-				<div class=" h-[110px]  w-[150px] rounded-2xl  bg-white ">
+				<div class=" h-[110px]  w-[165px] rounded-2xl  bg-white ">
 					<img
 						src="./images/georgia/parties/georgian_dream.svg"
-						class="mx-auto w-12 mt-4 rounded-full "
+						class="mx-auto mt-4 w-12 rounded-full "
 						alt=""
 					/>
-					<h3 class="mx-auto -mt-6 mb-4 text-center font-bold text-sm text-neutral-800">Georgian dream</h3>
+					<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
+						Georgian dream
+					</h3>
 				</div>
 
-				<div class=" h-[110px]  w-[150px] rounded-2xl  bg-white ">
+				<div class=" h-[110px]  w-[165px] rounded-2xl  bg-white ">
 					<img
 						src="./images/georgia/parties/georgian_dream.svg"
-						class="mx-auto w-12 mt-4 rounded-full "
+						class="mx-auto mt-4 w-12 rounded-full "
 						alt=""
 					/>
-					<h3 class="mx-auto -mt-6 mb-4 text-center font-bold text-sm text-neutral-800">Georgian dream</h3>
+					<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
+						Georgian dream
+					</h3>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class=" bg-white  p-6 ">
+	<div class=" bg-white  p-4 ">
 		{#each report as data}
 			<StatLine title={data.title} resultsProcents={data.resultsProcents} />
 		{/each}

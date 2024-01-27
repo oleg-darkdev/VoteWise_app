ef1a14b22ec0820756e0c57f6ebb03fc2efd8068
DarkDev <darkdev_brand@riseup.net>
2024-01-28 00:31:55 +0200

    refactoring landing components

---
+++ src/lib/widgets/ui/app/Result.svelte
@@ -0,0 +1,68 @@
+<script>
+	import ArrowLeft from 'carbon-icons-svelte/lib/Add.svelte';
+	import { StatLine } from '$lib/shared';
+
+	const report = [
+		{
+			title: 'Economics',
+			resultsProcents: [86, 20]
+		},
+		{
+			title: 'Social',
+			resultsProcents: [86, 20]
+		},
+		{
+			title: 'Political',
+			resultsProcents: [86, 20]
+		},
+
+	];
+	// export let data;
+</script>
+
+<div class="lg:px-4 py-8">
+	<!-- <button class="btn-square btn">
+		<ArrowLeft size={16}/>
+
+	</button> -->
+
+	<div
+		class=" prose-blue prose mx-auto flex flex-col  items-center rounded-t-2xl bg-neutral-200 px-6 py-8  lg:max-w-7xl lg:px-8"
+	>
+		<div class="mx-auto flex w-full max-w-3xl flex-col text-left">
+			<div class="mx-auto w-full ">
+				<h1 class="text-neutral-800">Here is your result.</h1>
+				<!-- <h2>A small headline to switch your visitors into users.</h2> -->
+				<p class="text-neutral-700">
+					Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
+					industries for previewing layouts and visual mockups.
+				</p>
+			</div>
+
+			<div class="flex flex-row justify-between">
+				<div class=" h-[110px]  w-[150px] rounded-2xl  bg-white ">
+					<img
+						src="./images/georgia/parties/georgian_dream.svg"
+						class="mx-auto w-12 mt-4 rounded-full "
+						alt=""
+					/>
+					<h3 class="mx-auto -mt-6 mb-4 text-center font-bold text-sm text-neutral-800">Georgian dream</h3>
+				</div>
+
+				<div class=" h-[110px]  w-[150px] rounded-2xl  bg-white ">
+					<img
+						src="./images/georgia/parties/georgian_dream.svg"
+						class="mx-auto w-12 mt-4 rounded-full "
+						alt=""
+					/>
+					<h3 class="mx-auto -mt-6 mb-4 text-center font-bold text-sm text-neutral-800">Georgian dream</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class=" bg-white  p-6 ">
+		{#each report as data}
+			<StatLine title={data.title} resultsProcents={data.resultsProcents} />
+		{/each}
+	</div>
+</div>
---
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -0,0 +1,27 @@
+<script>
+  
+</script>
+
+<div class=" w-full  max-w-2xl">
+	{#each faqContent as faq, i}
+		<div
+			on:click={() => {
+				faq.showFaq = !faq.showFaq;
+				console.log(faq.showFaq);
+			}}
+			class="collapse mb-1 w-full rounded-2xl bg-neutral-300 text-neutral-800"
+		>
+			<div class="collapse-title w-full text-xl font-medium">
+				{faq.question}
+			</div>
+
+			{#if faq.showFaq}
+				<div class="collapse-content w-full">
+					<p>
+						{faq.answer}
+					</p>
+				</div>
+			{/if}
+		</div>
+	{/each}
+</div>

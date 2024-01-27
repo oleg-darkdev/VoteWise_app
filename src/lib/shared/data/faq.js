501dd6b104e89755b8ff7a4f9fa35207e44afb7d
DarkDev <darkdev_brand@riseup.net>
2024-01-28 00:31:24 +0200

    refactoring structure of data

---
+++ src/lib/shared/data/faq.js
@@ -0,0 +1,32 @@
+const faqContent = [
+	{
+		question: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+		showFaq: false,
+		answer:
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+	},
+	{
+		question: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+		showFaq: false,
+
+		answer:
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+	},
+	{
+		question: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+		showFaq: false,
+
+		answer:
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+	},
+	{
+		question: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+		showFaq: false,
+
+		answer:
+			'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+	}
+];
+
+
+export default faqContent;
---
+++ src/lib/shared/data/quiz/questions.js
@@ -0,0 +1,55 @@
+const steps = [
+	{
+		id: 1,
+		title:
+			'Lorem ipsum is placeholder text.',
+		options: {
+			agree: {
+				value: '',
+				text: 'Аgree'
+			},
+
+			disagree: {
+				value: '',
+				text: 'Disagree'
+			}
+		},
+		answer: 0
+	},
+	{
+		id: 2,
+		title:
+			'Lorem ipsum is placeholder text.',
+		options: {
+			agree: {
+				value: '',
+				text: 'Аgree'
+			},
+
+			disagree: {
+				value: '',
+				text: 'Disagree'
+			}
+		},
+		answer: 0
+	},
+	{
+		id: 3,
+		title:
+			'Lorem ipsum is placeholder text.',
+		options: {
+			agree: {
+				value: '',
+				text: 'Аgree'
+			},
+
+			disagree: {
+				value: '',
+				text: 'Disagree'
+			}
+		},
+		answer: 0
+	}
+];
+
+export default steps;

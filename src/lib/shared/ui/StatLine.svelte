ccd78ce26bfcf99ecac7e6991f949675ad21cb92
DarkDev <darkdev_brand@riseup.net>
2024-01-28 01:14:00 +0200

    fix padding, add borders

--- src/lib/shared/ui/StatLine.svelte
+++ src/lib/shared/ui/StatLine.svelte
@@ -2,7 +2,7 @@
   export let title, resultsProcents;
 </script>
 
-<div class="mb-5 prose-blue prose lg:w-full w-full rounded-2xl border-2 border-neutral-200 px-4 py-6">
+<div class="mb-4 prose-blue prose lg:w-full w-full rounded-2xl border-2 border-neutral-200 px-4 py-6">
 	<h3 class="text-2xl text-neutral-800">{title}</h3>
 
 	<div class="mb-8 flex h-6 flex-row items-center">
@@ -11,7 +11,7 @@
 				<img src="./images/georgia/parties/georgian_dream.svg" />
 			</div>
 		</div>
-		<progress class="progress progress-primary mr-4 w-6/12 lg:w-full" value={resultsProcents[0]} max="100" />
+		<progress class="progress progress-primary mr-4 w-full" value={resultsProcents[0]} max="100" />
 		<p class="text-2xl font-thin text-neutral-800">{resultsProcents[0]}%</p>
 	</div>
 
@@ -21,7 +21,7 @@
 				<img src="./images/georgia/parties/united_national_movement.png" />
 			</div>
 		</div>
-		<progress class="progress progress-primary mr-4 w-6/12 lg:w-full" value={resultsProcents[1]} max="100" />
+		<progress class="progress progress-primary mr-4 w-full" value={resultsProcents[1]} max="100" />
 		<p class="text-2xl font-thin text-neutral-800">{resultsProcents[1]}%</p>
 	</div>
 </div>

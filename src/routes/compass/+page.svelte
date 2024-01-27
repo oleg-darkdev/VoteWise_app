35b50c463aa02df2f8164315e588209f3e4838cf
DarkDev <darkdev_brand@riseup.net>
2024-01-27 12:21:17 +0200

    routing

---
+++ src/routes/compass/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { wings } from '$lib/shared';
+</script>
+
+<!-- title: '',
+		desc: [''],
+		shortDesc: '',
+		img: '',
+		link: '' -->
+<!-- {#each wings as wing}
+
+
+
+{/each} -->

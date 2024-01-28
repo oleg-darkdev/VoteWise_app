9910aecc19c905231d8607adf8ba00265d264221
DarkDev <darkdev_brand@riseup.net>
2024-01-28 13:06:52 +0200

    fix width, add btn`s for popup

--- src/lib/widgets/ui/app/Result.svelte
+++ src/lib/widgets/ui/app/Result.svelte
@@ -1,5 +1,7 @@
 <script>
 	import ArrowLeft from 'carbon-icons-svelte/lib/ArrowLeft.svelte';
+	import ChevronRight from 'carbon-icons-svelte/lib/ChevronRight.svelte';
+
 	import { StatLine } from '$lib/shared';
 	import { fly } from 'svelte/transition';
 
@@ -10,19 +12,24 @@
 		},
 		{
 			title: 'Social',
-			resultsProcents: [86, 20]
+			resultsProcents: [43, 57]
 		},
 		{
 			title: 'Political',
-			resultsProcents: [86, 20]
+			resultsProcents: [21, 79]
 		}
 	];
+
+	let showPopUp = false;
 
 	export let startQuiz;
 </script>
 
-<div class="lg:py-8 lg:px-4 max-w-5xl" in:fly={{ x: 50, duration: 2000 }}
-	out:fly={{ x: -50, duration: 10 }}>
+<div
+	class="max-w-5xl lg:py-8 lg:px-4"
+	in:fly={{ x: 50, duration: 2000 }}
+	out:fly={{ x: -50, duration: 10 }}
+>
 	<div
 		class=" prose-blue prose mx-auto  items-center rounded-t-2xl bg-neutral-200 px-6 py-8   lg:px-8"
 	>
@@ -46,25 +53,49 @@
 			</div>
 
 			<div class="flex flex-row justify-between">
-				<div class=" h-[110px]  w-[165px] rounded-2xl  bg-white hover:bg-neutral-100">
+				<div
+					class=" h-[110px]  w-[145px] rounded-2xl bg-white  hover:bg-neutral-100  md:w-[165px] lg:w-[165px]"
+				>
 					<img
-						src="./images/georgia/parties/georgian_dream.svg"
-						class="mx-auto mt-4 w-12 rounded-full "
+						src="./images/georgia/georgian_dream.png"
+						class="mx-auto mt-4 w-8 rounded-full "
 						alt=""
 					/>
 					<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
-						Georgian dream
+						Georgian dream <br class="hidden lg:block" />
+
+						<div class="mx-auto mt-2 flex max-w-[80px] flex-row justify-center">
+							<button
+								on:click={() => (showPopUp = !showPopUp)}
+								class="-mt-1 mr-2 font-normal text-blue-700"
+								>details
+							</button>
+							<ChevronRight fill="#1d4ed8" size={16} class="" />
+						</div>
 					</h3>
 				</div>
 
-				<div class=" h-[110px]  w-[165px] rounded-2xl  bg-white hover:bg-neutral-100">
+				<div
+					class=" h-[110px]  w-[145px] rounded-2xl bg-white hover:bg-neutral-100  md:w-[165px] lg:w-[165px]"
+				>
 					<img
-						src="./images/georgia/parties/georgian_dream.svg"
-						class="mx-auto mt-4 w-12 rounded-full "
+						src="./images/georgia/united_national_movement.png"
+						class="mx-auto mt-4 w-8 rounded-full "
 						alt=""
 					/>
 					<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
-						Georgian dream
+						<!-- United National Movement -->
+						U.N.M.
+						<br class="hidden lg:block" />
+
+						<div class="mx-auto mt-2 flex max-w-[80px] flex-row justify-center">
+							<button
+								on:click={() => (showPopUp = !showPopUp)}
+								class="-mt-1 mr-2 font-normal text-blue-700"
+								>details
+							</button>
+							<ChevronRight fill="#1d4ed8" size={16} class="" />
+						</div>
 					</h3>
 				</div>
 			</div>

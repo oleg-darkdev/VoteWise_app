57c2611ef36ecdba797e908a2c7723d7eb3585a9
DarkDev <darkdev_brand@riseup.net>
2024-01-28 11:33:15 +0200

    upd content in faq

--- src/lib/shared/data/faq.js
+++ src/lib/shared/data/faq.js
@@ -1,27 +1,27 @@
 const faqContent = [
 	{
 		question:
-			'How does your product help users to understand political positions and programs of parties in Georgia?',
+			`How does your product help users to understand political positions and programs of parties in Georgia?`,
 		showFaq: false,
-		answer: `We provide detailed profiles and analysis of political parties' platforms, including core positions on important topics, proposals on social policy, economic strategies and other aspects.`
+		answer: `We provide detailed profiles and analysis of political parties platforms, including core positions on important topics, proposals on social policy, economic strategies and other aspects.`
 	},
 	{
 		question:
-			'What is the main goal of the product to help users navigate through the political parties in Georgia?',
+			`What is the main goal of the product to help users navigate through the political parties in Georgia?`,
 		showFaq: false,
 
 		answer:
-			'We aim to simplify the process of making informed decisions when choosing political representatives by providing detailed and objective information about different political parties.'
+			`We aim to simplify the process of making informed decisions when choosing political representatives by providing detailed and objective information about different political parties.`
 	},
 	{
 		question:
-			'Can I get personalized recommendations on suitable political parties based on my views and values?',
+			`Can I get personalized recommendations on suitable political parties based on my views and values?`,
 		showFaq: false,
 
 		answer: `Yes, of course! Our service offers an interactive tool known as "Political Compass" that analyzes your views and values. Based on this analysis, we provide you with personalized recommendations on political parties in Georgia that best match your beliefs.`
 	},
 	{
-		question: 'How regularly is the information on political parties in your product updated?',
+		question: `How regularly is the information on political parties in your product updated?`,
 		showFaq: false,
 
 		answer: `We are dedicated to providing up-to-date information on political parties in Georgia. Our database is updated before every election, and the latest data becomes publicly available 4 months before the start of the election process.`

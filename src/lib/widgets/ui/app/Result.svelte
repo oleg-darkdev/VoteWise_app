01f709e72e5a07f6069f07c73268650f4b817cb2
DarkDev <darkdev_brand@riseup.net>
2024-01-28 13:20:40 +0200

    refactoring

--- src/lib/widgets/ui/app/Result.svelte
+++ src/lib/widgets/ui/app/Result.svelte
@@ -7,20 +7,20 @@
 
 	const report = [
 		{
-			title: 'Economics',
+			title: 'Economic issues',
 			resultsProcents: [86, 20]
 		},
 		{
-			title: 'Social',
+			title: 'Social issues',
 			resultsProcents: [43, 57]
 		},
 		{
-			title: 'Political',
+			title: 'Political issues',
 			resultsProcents: [21, 79]
 		}
 	];
 
-	let showPopUp = false;
+	let showPopUp = true;
 
 	export let startQuiz;
 </script>
@@ -47,63 +47,79 @@
 				<h1 class="mb-0 text-neutral-800">Here is your result.</h1>
 				<!-- <h2>A small headline to switch your visitors into users.</h2> -->
 				<p class="text-neutral-700">
-					Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
-					industries for previewing layouts and visual mockups.
+					<span class="font-bold">Following 3 issues</span> show which electoral program of the political party is closest to you
 				</p>
 			</div>
+			{#if !showPopUp}
+				<div class="flex flex-row justify-between">
+					<div
+						class=" h-[110px]  w-[145px] rounded-2xl bg-white  hover:bg-neutral-100  md:w-[165px] lg:w-[165px]"
+					>
+						<img
+							src="./images/georgia/georgian_dream.png"
+							class="mx-auto mt-4 w-8 rounded-full "
+							alt=""
+						/>
+						<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
+							Georgian dream <br class="hidden lg:block" />
 
-			<div class="flex flex-row justify-between">
-				<div
-					class=" h-[110px]  w-[145px] rounded-2xl bg-white  hover:bg-neutral-100  md:w-[165px] lg:w-[165px]"
-				>
+							<div class="mx-auto mt-2 flex max-w-[80px] flex-row justify-center">
+								<button
+									on:click={() => (showPopUp = !showPopUp)}
+									class="-mt-1 mr-2 font-normal text-blue-700"
+									>details
+								</button>
+								<ChevronRight fill="#1d4ed8" size={16} class="" />
+							</div>
+						</h3>
+					</div>
+
+					<div
+						class=" h-[110px]  w-[145px] rounded-2xl bg-white hover:bg-neutral-100  md:w-[165px] lg:w-[165px]"
+					>
+						<img
+							src="./images/georgia/united_national_movement.png"
+							class="mx-auto mt-4 w-8 rounded-full "
+							alt=""
+						/>
+						<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
+							<!-- United National Movement -->
+							U.N.M.
+							<br class="hidden lg:block" />
+
+							<div class="mx-auto mt-2 flex max-w-[80px] flex-row justify-center">
+								<button
+									on:click={() => (showPopUp = !showPopUp)}
+									class="-mt-1 mr-2 font-normal text-blue-700"
+									>details
+								</button>
+								<ChevronRight fill="#1d4ed8" size={16} class="" />
+							</div>
+						</h3>
+					</div>
+				</div>
+			{:else}
+				<div class="flex w-full flex-row rounded-t-2xl bg-white px-4">
 					<img
 						src="./images/georgia/georgian_dream.png"
-						class="mx-auto mt-4 w-8 rounded-full "
+						class=" mt-4 w-16 rounded-full mr-4"
 						alt=""
 					/>
-					<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
-						Georgian dream <br class="hidden lg:block" />
-
-						<div class="mx-auto mt-2 flex max-w-[80px] flex-row justify-center">
-							<button
-								on:click={() => (showPopUp = !showPopUp)}
-								class="-mt-1 mr-2 font-normal text-blue-700"
-								>details
-							</button>
-							<ChevronRight fill="#1d4ed8" size={16} class="" />
-						</div>
-					</h3>
+					<div class="prose-blue prose pt-3">
+						<h3 class=" text-center text-md font-bold text-neutral-800">
+							Georgian dream
+						</h3>
+						<span class='-mt-4'>since 2010</span>
+					</div>
 				</div>
-
-				<div
-					class=" h-[110px]  w-[145px] rounded-2xl bg-white hover:bg-neutral-100  md:w-[165px] lg:w-[165px]"
-				>
-					<img
-						src="./images/georgia/united_national_movement.png"
-						class="mx-auto mt-4 w-8 rounded-full "
-						alt=""
-					/>
-					<h3 class="mx-auto -mt-6 mb-4 text-center text-sm font-bold text-neutral-800">
-						<!-- United National Movement -->
-						U.N.M.
-						<br class="hidden lg:block" />
-
-						<div class="mx-auto mt-2 flex max-w-[80px] flex-row justify-center">
-							<button
-								on:click={() => (showPopUp = !showPopUp)}
-								class="-mt-1 mr-2 font-normal text-blue-700"
-								>details
-							</button>
-							<ChevronRight fill="#1d4ed8" size={16} class="" />
-						</div>
-					</h3>
-				</div>
-			</div>
+			{/if}
 		</div>
 	</div>
-	<div class=" bg-white  p-4 ">
-		{#each report as data}
-			<StatLine title={data.title} resultsProcents={data.resultsProcents} />
-		{/each}
-	</div>
+	{#if !showPopUp}
+		<div class=" bg-white  p-4 ">
+			{#each report as data}
+				<StatLine title={data.title} resultsProcents={data.resultsProcents} />
+			{/each}
+		</div>
+	{/if}
 </div>

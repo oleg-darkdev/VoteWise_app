44ebe435ddd6e9f94b1673dd1f5f007f2990b385
DarkDev <darkdev_brand@riseup.net>
2024-05-03 15:13:01 +0300

    upd api

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -3,6 +3,7 @@ import Faq from './ui/landing/Faq.svelte';
 import Hero from './ui/landing/Hero.svelte';
 import Result from './ui/app/Result.svelte';
 import Team from './ui/landing/Team.svelte';
+import Footer from './ui/landing/Footer.svelte';
 import DetailedAboutParty from './ui/app/DetailedAboutParty.svelte';
 
-export { Quiz, DetailedAboutParty, Faq, Hero, Result, Team };
+export { Quiz, DetailedAboutParty, Footer, Faq, Hero, Result, Team };

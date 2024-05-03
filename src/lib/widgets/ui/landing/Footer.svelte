f19cec9a469c597d74c63ed9ce8d58278d43fac2
DarkDev <darkdev_brand@riseup.net>
2024-05-03 15:12:41 +0300

    add darkdev logo

---
+++ src/lib/widgets/ui/landing/Footer.svelte
@@ -0,0 +1,17 @@
+<footer class='w-full py-6 bg-neutral-400 grid grid-cols-1'>
+  <div class="mx-auto">
+    <h2 class='text-xs text-gray-900 '>Development by</h2>
+    <a href="https://darkdev-progect.vercel.app/" target="_blank">    <img src="/images/darkdev.svg"  class='h-20' alt="darkdev logo">
+</a>
+  </div>
+</footer>
+
+
+<style>
+  h2 {
+    text-transform: uppercase;
+  font-weight: 900;
+  letter-spacing: 1em;
+  margin-left: 1em;
+  }
+</style>

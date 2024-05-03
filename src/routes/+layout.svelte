79ffaab28c959d87ba145fb174b288eed5472d6d
DarkDev <darkdev_brand@riseup.net>
2024-05-03 15:31:31 +0300

    add page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,16 @@
 	import '../app.css';
 	import 'aos/dist/aos.css';
 	import AOS from 'aos';
-  
+
 	import { onMount } from 'svelte';
 	onMount(() => {
 		AOS.init();
 	});
 </script>
+
+<svelte:head>
+	<title>VoteWise in Georgia</title>
+</svelte:head>
 
 <main class="">
 	<slot />
